f5c73e5c471f433ffcb4011dbfe67261ca2bb8c6
Julian Cordes <julian.cordes@gmail.com>
2014-01-15 00:23:55 +0100

    camera added

---
+++ camera.cpp
@@ -0,0 +1,133 @@
+#include "camera.h"
+
+#include "header.h"
+
+Camera::Camera() {
+	rotX = 0.0f;
+	rotY = 0.0f;	
+	rotZ = 0.0f;
+	setPos(0.0f, 0.0f, 70.0f);
+	setPerspective(45.0f, 1.0f, 0.1f, 200.0f );
+	
+	ViewRotateX = glm::rotate(glm::mat4(1.0f), rotX, glm::vec3(1.0f, 0.0f, 0.0f));
+	ViewRotateXY = glm::rotate(ViewRotateX, rotY, glm::vec3(0.0f, 1.0f, 0.0f));
+	View = glm::translate(ViewRotateXY,glm::vec3(posX, posY, posZ));
+	Model = glm::scale(glm::mat4(1.0f),glm::vec3(1.0f));
+	
+	MVP = Projection * View * Model;
+	
+}
+
+Camera::~Camera() {
+
+}
+
+void Camera::incRotateX(float incValue) {
+	this->rotX += incValue;
+	recalcRotateView();
+}
+
+void Camera::incRotateY(float incValue) {
+	this->rotY += incValue;
+	recalcRotateView();
+}
+
+void Camera::incRotateZ(float incValue) {
+	this->rotZ += incValue;
+	recalcRotateView();
+}
+
+void Camera::setRotateX (float rotX) {
+	this->rotX = rotX;
+	recalcRotateView();
+}
+
+void Camera::setRotateY (float rotY) {
+	this->rotY = rotY;
+	recalcRotateView();
+}
+
+void Camera::setRotateZ (float rotZ) {
+	this->rotZ = rotZ;
+	recalcRotateView();
+}
+
+void Camera::setPosX (float x) {
+	this->posX = x;
+	recalcView();
+}
+
+void Camera::setPosY (float y) {
+	this->posY = y;
+	recalcView();
+}
+
+void Camera::setPosZ (float z) {
+	this->posZ = z;
+	recalcView();
+}
+
+void Camera::setPos (float x, float y, float z) {
+	this->posX = x;
+	this->posY = y;
+	this->posZ = z;
+	recalcView();
+}
+
+float Camera::getX() {
+	return this->posX;
+}
+
+float Camera::getY() {
+	return this->posY;
+}
+
+float Camera::getZ() {
+	return this->posZ;
+}
+
+void Camera::setPerspective(float angle, float ratio, float nearClip, float farClip) {
+	Projection = glm::perspective(angle, ratio, nearClip, farClip);
+	MVP = Projection * View * Model;
+}
+
+void Camera::recalcView() {
+	View = glm::translate(ViewRotateXY,glm::vec3(-posX, -posY, -posZ));
+	
+	MVP = Projection * View * Model;
+}
+
+void Camera::recalcRotateView() {
+	ViewRotateX = glm::rotate(glm::mat4(1.0f), rotX, glm::vec3(1.0f, 0.0f, 0.0f));
+	ViewRotateXY = glm::rotate(ViewRotateX, rotY, glm::vec3(0.0f, 1.0f, 0.0f));
+	View = glm::translate(ViewRotateXY,glm::vec3(-posX, -posY, -posZ));
+	
+	MVP = Projection * View * Model;
+}
+
+void Camera::moveForward(float length) {
+	vec4 posVec = vec4(0.0f, 0.0f, length, 0.0f) * this->getViewRotateXY();
+
+	posX += posVec.x;
+	posY += posVec.y;
+	posZ += posVec.z;
+	
+	recalcView();
+}
+void Camera::moveSideward(float length) {
+	vec4 posVec = vec4(length, 0.0f, 0.0f, 0.0f) * this->getViewRotateXY();
+	
+	posX += posVec.x;
+	posY += posVec.y;
+	posZ += posVec.z;
+	
+	recalcView();
+}
+
+mat4& Camera::getMVP() {
+	return this->MVP;
+}
+
+mat4& Camera::getViewRotateXY() {
+	return this->ViewRotateXY;
+}
---
+++ camera.h
@@ -0,0 +1,61 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+using namespace std;
+using namespace glm;
+
+class Camera {
+	public:
+	Camera();
+	~Camera();
+	
+	void incRotateX(float incValue);
+	void incRotateY(float incValue);
+	void incRotateZ(float incValue);
+	
+	void setRotateX(float rotX);
+	void setRotateY(float rotY);
+	void setRotateZ(float rotZ);
+
+	void setPosX(float x);
+	void setPosY(float y);
+	void setPosZ(float z);
+	
+	void setPos(float x, float y, float z);
+		
+	mat4& getViewRotateXY();
+	void setPerspective(float angle, float ratio, float nearClip, float farClip);
+	
+	void moveForward(float length);
+	void moveSideward(float length);
+	
+	float getX();
+	float getY();
+	float getZ();
+	
+	mat4& getMVP();
+
+	private:
+	
+	mat4 Projection;
+	mat4 ViewRotateX;
+	mat4 ViewRotateXY;
+	mat4 View;
+	mat4 Model;	
+	
+	mat4 MVP;
+	
+	float rotX, rotY, rotZ;
+	
+	float posX, posY, posZ;
+	
+	void recalcView();
+	void recalcRotateView();
+	
+};
+
+#endif
